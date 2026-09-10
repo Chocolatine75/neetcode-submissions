e60ef7ea0cd06d9f1627871e36a8d962466bbe09
Matteo ATKINSON <73189720+Chocolatine75@users.noreply.github.com>
2026-09-10 15:20:15 +0200

    Add: reverse-integer - submission-5

---
+++ Data Structures & Algorithms/reverse-integer/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def reverse(self, x: int) -> int:
+        res = 0
+        sign = False
+        if x<0:
+            sign = True
+            x = -x
+        while x>0:
+            res += x%10
+            x= x//10
+            if 2147483647 <= res :
+                return 0
+            
+            res*=10
+
+        res =res//10
+        if sign:
+            res = -res
+        # if -2**31 > res or res > 2**31 - 1:
+        #         return 0
+      
+        return res
+
